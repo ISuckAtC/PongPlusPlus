b6ea97bf83606f35fbee7b42e64a9f3d7c1511f5
Ross <theporkchopsxd@gmail.com>
2020-09-10 01:46:38 +0200

    Initial

---
+++ Assets/Scripts/PlatformMovement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour
+{
+    // Public keycode properties allows you to set the keys in editor for easy testing
+    public KeyCode Up, Down;
+    public float speed;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey(Up)) 
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+        }
+        
+        if(Input.GetKey(Down))
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * speed);
+        }
+    }
+}
