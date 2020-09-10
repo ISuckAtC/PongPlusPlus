0e40e9f1329508849ee1ffe7ef2ece4a2e0e3fcf
Mikkel <mikkelking97@gmail.com>
2020-09-10 17:19:51 +0200

    Fixed movement

--- Assets/Scripts/HorizontalPlatformMovement.cs
+++ Assets/Scripts/HorizontalPlatformMovement.cs
@@ -2,10 +2,10 @@
 using System.Collections;
 using UnityEngine;
 
-public class PlatformMovement : MonoBehaviour
+public class HorizontalPlatformMovement : MonoBehaviour
 {
     // Public keycode properties allows you to set the keys in editor for easy testing
-    public KeyCode Up, Down;
+    public KeyCode Left, Right;
     public float speed;
 
     Rigidbody2D rb;
@@ -18,13 +18,13 @@ public class PlatformMovement : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKey(Up)) 
+        if (Input.GetKey(Right)) 
         {
-            rb.velocity = new Vector2(0, speed);
+            rb.velocity = new Vector2(speed, 0);
         }
-        else if(Input.GetKey(Down))
+        else if(Input.GetKey(Left))
         {
-            rb.velocity = new Vector2(0, -speed);
+            rb.velocity = new Vector2(-speed, 0);
         } else rb.velocity = Vector2.zero;
     }
 }
---
+++ Assets/Scripts/VerticalPlatformMovement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class VerticalPlatformMovement : MonoBehaviour
+{
+    // Public keycode properties allows you to set the keys in editor for easy testing
+    public KeyCode Up, Down;
+    public float speed;
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey(Up)) 
+        {
+            rb.velocity = new Vector2(0, speed);
+        }
+        else if(Input.GetKey(Down))
+        {
+            rb.velocity = new Vector2(0, -speed);
+        } else rb.velocity = Vector2.zero;
+    }
+}
