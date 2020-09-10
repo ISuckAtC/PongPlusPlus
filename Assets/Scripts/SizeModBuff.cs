7d73253d3ed61e826e5192eb14780f0bb115e0e9
CBBA <theporkchopsxd@gmail.com>
2020-09-10 16:19:37 +0100

    Added size mod buff script

---
+++ Assets/Scripts/SizeModBuff.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizeModBuff : MonoBehaviour
+{
+    public float SizeMod;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Ball")
+        {
+            collider.transform.localScale.Scale(new Vector3(SizeMod, SizeMod, 1));
+            Destroy(gameObject);
+        }
+    }
+}
