fd1b88af388dca8458539cca6fe25b4f8b060e94
Ross <theporkchopsxd@gmail.com>
2020-09-28 21:38:59 +0200

    Initial prototype 2

---
+++ Assets/Scripts/DeathBarrierBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathBarrierBehavior : MonoBehaviour
+{
+    public GameObject player;
+
+    Collider2D col;
+    SpriteRenderer sr;
+
+    public Material full;
+    public Material faded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = GetComponent<Collider2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Ball")
+        {
+
+            col.isTrigger = false;
+            Destroy(collider.gameObject);
+            Destroy(player);
+            sr.material = full;
+        }
+    }
+}
