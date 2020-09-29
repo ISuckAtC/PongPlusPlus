2ae7c405fff2618d48d3af6e76c4cb9eeb5a1b77
Mikkel <mikkelking97@gmail.com>
2020-09-29 13:10:00 +0200

    KillcounterUnderProgress

--- Assets/Scripts/PlayerCardBehavior.cs
+++ Assets/Scripts/PlayerCardBehavior.cs
@@ -5,8 +5,8 @@ using UnityEngine.UI;
 public class PlayerCardBehavior : MonoBehaviour
 {
     int[] playerBallCount;
-
-    public void UIUpdate(int amount, int player)
+    int[] playerKillCount;
+    public void BallCountUpdate(int amount, int player)
     {
         if (player < 0 || player > 3) throw new ArgumentOutOfRangeException("player", player, "player must be between 0 and 3");
         playerBallCount[player] += amount;
@@ -16,9 +16,20 @@ public class PlayerCardBehavior : MonoBehaviour
         card.transform.Find("Balls").GetComponent<Text>().text = "Balls: " + playerBallCount[player];
     }
 
+    public void KillCountUpdate(int amount, int player)
+    {
+        if (player < 0 || player > 3) throw new ArgumentOutOfRangeException("player", player, "player must be between 0 and 3");
+        playerKillCount[player] += amount;
+
+        GameObject card = transform.GetChild(player).gameObject;
+
+        card.transform.Find("Kills").GetComponent<Text>().text = "Kills:" + playerKillCount[player];
+    }
     void Start()
     {
         playerBallCount = new int[] {0, 0, 0, 0};
+
+        playerKillCount = new int[] {0, 0, 0, 0};
     }
 
     // Update is called once per frame
@@ -26,7 +37,12 @@ public class PlayerCardBehavior : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.U))
         {
-            UIUpdate(1, 0);
+            BallCountUpdate(1, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            KillCountUpdate(1, 0);
         }
     }
 }
