adf9fa4387ae3ffeb295179d58f07add076aa896
Darja <sindarja01@gmail.com>
2020-10-09 12:52:43 +0200

    working scene swithces

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        Application.Quit(0);
+    }
+    public void PlayMenu()
+    {
+        SceneManager.LoadScene("Menu_Screen", LoadSceneMode.Single);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
