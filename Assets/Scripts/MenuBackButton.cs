4f40e531c84519bb69422d5342d25f739efc001a
Mikkel <mikkelking97@gmail.com>
2020-10-14 11:41:28 +0200

    MenuStuff

---
+++ Assets/Scripts/MenuBackButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuBackButton : MonoBehaviour
+{
+
+ public void BackButton()
+    {
+        SceneManager.LoadScene("Prototype 2 Darja", LoadSceneMode.Single);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
