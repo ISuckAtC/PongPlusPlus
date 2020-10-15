b0ae38c13c98e6813d9378c17430f43a1a331885
Mikkel <mikkelking97@gmail.com>
2020-10-15 20:06:00 +0200

    MenuPlayerSelection Stuff

--- Assets/Scripts/PlayerSelectMenu.cs
+++ Assets/Scripts/PlayerSelectMenu.cs
@@ -13,10 +13,47 @@ public class PlayerSelectMenu : MonoBehaviour
     {
         SceneManager.LoadScene("Prototype 3");
     }
+
+    public GameObject Player1, Player2, Player3, Player4, Player2Button, Player3Button, Player4Button;
+    
+    public void OnPlayer2Click()
+    {
+        Player2Button.SetActive(false);
+        Player3Button.SetActive(false);
+        Player4Button.SetActive(false);
+
+        Player1.SetActive(true);
+        Player2.SetActive(true);
+    }
+
+    public void OnPlayer3Click()
+    {
+        Player2Button.SetActive(false);
+        Player3Button.SetActive(false);
+        Player4Button.SetActive(false);
+
+        Player1.SetActive(true);
+        Player2.SetActive(true);
+        Player3.SetActive(true);
+    }
+
+    public void OnPlayer4Click()
+    {
+        Player2Button.SetActive(false);
+        Player3Button.SetActive(false);
+        Player4Button.SetActive(false);
+
+        Player1.SetActive(true);
+        Player2.SetActive(true);
+        Player3.SetActive(true);
+        Player4.SetActive(true);
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Player1.SetActive(false);
     }
 
     // Update is called once per frame
