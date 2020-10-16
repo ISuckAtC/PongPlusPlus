6f6422cacb9e9070ff91f857b37b2f6fe62a4ae1
Darja <sindarja01@gmail.com>
2020-10-16 08:21:58 +0200

    unfinished settings menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,14 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    //Exit button should exit game
     public void ExitGame()
     {
         Application.Quit(0);
     }
+    //Switches scene from main menu to player select menu
     public void PlayMenu()
     {
         SceneManager.LoadScene("Menu_Screen", LoadSceneMode.Single);
     }
+    
+
     // Start is called before the first frame update
     void Start()
     {
