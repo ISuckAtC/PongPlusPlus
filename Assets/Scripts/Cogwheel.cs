f80696b98e1df6aff61b6f2d115cc5e1646befda
CBBA <theporkchopsxd@gmail.com>
2020-11-06 14:38:15 +0100

    Cogwheel obstacle

---
+++ Assets/Scripts/Cogwheel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cogwheel : MonoBehaviour
+{
+    public float RotateSpeed;
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, RotateSpeed);
+    }
+}
