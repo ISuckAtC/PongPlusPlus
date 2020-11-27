e27461aba5a84fc7a54b2bb98adaddee36159f11
Darja <sindarja01@gmail.com>
2020-11-27 13:43:10 +0100

    taken thing

---
+++ Assets/Scripts/Flaps.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flap : MonoBehaviour
+{
+    public Transform TopPivot, BotPivot, LeftPivot, RightPivot;
+    public float HowMuchRotate;
+    public float MaxRotate;
+    public bool ClockWise;
+    public bool taken; //If currently used by player
+
+    public void ToMoveFlap(object sender, EventArgs e)
+    {
+        transform.Rotate(new Vector3 (0, 0, HowMuchRotate));
+    }
+    public void AssignPlayer(BasePlatformMovement platmove)
+    {
+        platmove.FirePositive += ToMoveFlap;
+    }
+}
